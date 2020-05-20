d4fa3c9faed3b0a6ef1d91cb768d4df7ca6d7ad6
TimOgden <togden1999@gmail.com>
2020-05-19 21:43:57 -0400

    Added progress bars and automatic choosing of the most efficient algorithm

--- image_detector.py
+++ image_detector.py
@@ -2,31 +2,31 @@ import cv2
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
-
+from tqdm import tqdm
 def partition_slow(img):
-	for r in range(img.shape[0]):
-		for c in range(img.shape[1]):
-			r_index = int(img[r][c][0] / 256 * NUM_PARTITIONS)
-			g_index = int(img[r][c][1] / 256 * NUM_PARTITIONS)
-			b_index = int(img[r][c][2] / 256 * NUM_PARTITIONS)
-			if (r_index, g_index, b_index) in partitions_sums:
-				partitions_sums[(r_index, g_index, b_index)] += 1
-			else:
-				partitions_sums[(r_index, g_index, b_index)] = 1
+	with tqdm(total=img.shape[0]*img.shape[1]) as pbar:
+		for r in range(img.shape[0]):
+			for c in range(img.shape[1]):
+				r_index = int(img[r][c][0] / 256 * NUM_PARTITIONS)
+				g_index = int(img[r][c][1] / 256 * NUM_PARTITIONS)
+				b_index = int(img[r][c][2] / 256 * NUM_PARTITIONS)
+				if (r_index, g_index, b_index) in partitions_sums:
+					partitions_sums[(r_index, g_index, b_index)] += 1
+				else:
+					partitions_sums[(r_index, g_index, b_index)] = 1
+				pbar.update(1)
 
 def partition_fast(img):
 	img = img.astype(np.float32)
 	img /= 256.
 	img *= NUM_PARTITIONS
 	img = img.astype(np.int8)
-	print(img[10][25])
-	print('Shape:', img.shape)
-	offset = 256/(NUM_PARTITIONS*2)
-	for r in range(NUM_PARTITIONS):
-		for g in range(NUM_PARTITIONS):
-			for b in range(NUM_PARTITIONS):
-				#print(r,g,b)
-				partitions_sums[(r,g,b)] = (img==(r,g,b)).all(axis=-1).sum()
+	with tqdm(total=NUM_PARTITIONS**3) as pbar:
+		for r in range(NUM_PARTITIONS):
+			for g in range(NUM_PARTITIONS):
+				for b in range(NUM_PARTITIONS):
+					partitions_sums[(r,g,b)] = (img==(r,g,b)).all(axis=-1).sum()
+				pbar.update(NUM_PARTITIONS)
 
 
 NUM_PARTITIONS = int(sys.argv[2]) # This will be how many times the color line will be divided.
@@ -37,7 +37,12 @@ print(img.shape)
 plt.imshow(img)
 plt.show()
 partitions_sums = {}
-partition_fast(img)
+if NUM_PARTITIONS**3 < img.shape[0]*img.shape[1]:
+	print('Computing distribution of each partition using numpy array element-wise functions...')
+	partition_fast(img)
+else:
+	print('Computing distribution of each partition by looping through the entire image...')
+	partition_slow(img)
 
 
 
